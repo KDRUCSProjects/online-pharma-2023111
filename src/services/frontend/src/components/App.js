7bcf3d4a3a2f206f4755b9bf7ebe95e0308aeef6
Mansoor Faizi <mansoorfaizi.cs05@gmail.com>
2023-06-04 11:06:22 +0430

    feat/ui/product-details: product details page added

--- src/services/frontend/src/components/App.js
+++ src/services/frontend/src/components/App.js
@@ -2,16 +2,24 @@ import React from 'react';
 import NavBar from './navbar/NavBar';
 import { Box, Container, Grid } from '@mui/material';
 import { Outlet } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const App = () => {
+    const queryClient = new QueryClient({
+        defaultOptions: {},
+    });
     return (
         <Box sx={{ flexGrow: 1 }}>
-            <NavBar />
-            <Container>
-                <Box>
-                    <Outlet />
-                </Box>
-            </Container>
+            <QueryClientProvider client={queryClient}>
+                <NavBar />
+                <Container>
+                    <Box>
+                        <Outlet />
+                    </Box>
+                </Container>
+                <ReactQueryDevtools initialIsOpen />
+            </QueryClientProvider>
         </Box>
     );
 };
--- src/services/frontend/src/index.js
+++ src/services/frontend/src/index.js
@@ -4,6 +4,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './components/App';
 import SignUp from './components/user/SignUp';
 import Login from './components/user/login';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import ProductDetails from './components/ProductDetails';
 
 const router = createBrowserRouter([
     {
@@ -17,12 +19,22 @@ const router = createBrowserRouter([
     },
     {
         path: '/login/',
-        element: <Login />
+        element: <Login />,
+    },
+    {
+        path: 'details/',
+        element: <ProductDetails />
     }
 ]);
 
+const queryClient = new QueryClient({
+    defaultOptions: {},
+});
+
 ReactDOM.createRoot(document.getElementById('app')).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+        </QueryClientProvider>
     </React.StrictMode>
 );
