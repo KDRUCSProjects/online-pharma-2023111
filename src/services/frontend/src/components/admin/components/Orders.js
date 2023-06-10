0bb465123710f0b69c1038bffbeb63c7ca48df8a
Mansoor Faizi <mansoorfaizi.cs05@gmail.com>
2023-06-10 19:49:08 +0430

    feat/ui/dashboard: created layout for admin

---
+++ src/services/frontend/src/components/admin/components/Orders.js
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import Link from '@mui/material/Link';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Title from './Title';
+
+// Generate Order Data
+function createData(id, date, name, drug_name, quantity, price, total) {
+  return { id, date, name, drug_name, quantity, price, total };
+}
+
+const rows = [
+  createData(
+    0,
+    '16 Mar, 2023',
+    'Mansoor',
+    'Prostamol',
+    3,
+    200,
+    600
+  ),
+];
+
+function preventDefault(event) {
+  event.preventDefault();
+}
+
+export default function Orders() {
+  return (
+    <React.Fragment>
+      <Title>Recent Drug Orders</Title>
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell>Date</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Drug Name</TableCell>
+            <TableCell>Quantity</TableCell>
+            <TableCell>Price</TableCell>
+            <TableCell align='right'>Total</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.id}>
+              <TableCell>{row.date}</TableCell>
+              <TableCell>{row.name}</TableCell>
+              <TableCell>{row.drug_name}</TableCell>
+              <TableCell>{row.quantity}</TableCell>
+              <TableCell>{row.price}</TableCell>
+              <TableCell align="right">{`$${row.total}`}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
+        See more orders
+      </Link>
+    </React.Fragment>
+  );
+}
