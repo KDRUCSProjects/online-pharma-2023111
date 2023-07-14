5a7f68ec01a6face49759d8cde3f97c89ae814d5
Kaihan <niamat>
2023-07-14 12:50:08 +0430

    fix/ui/slider:fixed slider ,images added for slider

---
+++ src/services/frontend/src/components/layout/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Slider from '../slider/Slider'
+import Category from '../category/Category'
+import { Box } from '@mui/material'
+
+const Home = () => {
+  return (
+    <Box>
+        <Slider />
+        <Category />
+    </Box>
+  )
+}
+
+export default Home
